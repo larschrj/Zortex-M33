0feb756d3bba1f0a8c0d8aa43a4d49eb9e836b96
Lars Johansen <larschrj@users.noreply.github.com>
2026-04-12 15:37:50 +0200

    Add types to Vl53l5 driver
    Reformat firmware arrays

--- driver/vl53l5/root.zig
+++ driver/vl53l5/root.zig
@@ -1,1 +1,28 @@
+const Vl53l5 = @This();
 
+pub const I2c_addr = enum(u8) {
+    @"0x44" = 0x44,
+    @"0x45" = 0x45,
+    @"0x46" = 0x46,
+    @"0x47" = 0x47,
+};
+
+pub const Error = error{
+    Busy,
+    BusError,
+    ArbitrationLost,
+    NotAcknowledge,
+    MeasurementNotReady,
+};
+
+const ReadFunc = ?*const fn (addr: u10, receive_data: []u8, start: bool, stop: bool, reload: bool) Error!void;
+const WriteFunc = ?*const fn (addr: u10, transmit_data: []const u8, start: bool, stop: bool, reload: bool) Error!void;
+
+init: bool = false,
+addr: u8 = undefined,
+read_func: ReadFunc = null,
+write_func: WriteFunc = null,
+measurement_mode: MeasurementMode = undefined,
+low_power_mode: LowPowerMode = undefined,
+
+pub fn initialize() void {}
